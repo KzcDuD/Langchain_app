961d723892d6e72d612e13fc3e466e6c0ab3cc1f
yungchih0209 <chih105015@gmail.com>
2023-12-11 23:34:27 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def generate_pet_name(animal_type,pet_color):
+    llm =OpenAI(temperature =0.7) # The model creative temperature
+    prompt_template_name = PromptTemplate(
+        input_variables =['animal_type'],
+        template="I have a {animal_type} pet and I want a coll name for it , it is {pet_color} in color. Suggest me five cool names for my pet."
+    )
+    name_chain = LLMChain(llm=llm,prompt=prompt_template_name)
+    response =  name_chain({'animal_type':animal_type,'pet_color':pet_color})
+    
+    return response
+    
+if __name__ == "__main__":
+    print(generate_pet_name("cat","balck"))
