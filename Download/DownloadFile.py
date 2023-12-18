0a0ec7ae76d513a7b4744b11ee62686bf02fa4b2
yungchih0209 <chih105015@gmail.com>
2023-12-19 01:59:33 +0800

    Update DownloadFile.py

--- Download/DownloadFile.py
+++ Download/DownloadFile.py
@@ -1,12 +1,5 @@
 import gdown
 
-url = "https://drive.google.com/drive/u/0/folders/1f1GxaU8RbP6-AWGfgFYxGjH2u-vGLQEG"
-output = "fcn8s_from_caffe.npz"
+url = 'https://drive.google.com/drive/u/0/folders/1KDR00nXZiKuNt_pWq5MHhHNxgeJMS-7h'
 
-gdown.download_folder(url, quiet=True, use_cookies=False)
-
-
-print('Folder downloaded successfully.')
-
-
-############################################################################################################
+gdown.download_folder(url)
